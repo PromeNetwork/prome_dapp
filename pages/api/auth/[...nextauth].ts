750e67e6c2114640d44c0cb6aceaa93e7c2fef7b
libaozhong <baozhongmail@yeah.net>
2024-03-17 17:10:34 +0800

    add face book

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,70 @@
-import NextAuth ,{type CallbacksOptions,type Profile,type Account}from "next-auth"
+import NextAuth ,{type CallbacksOptions,type Profile,type Account,type User,Session}from "next-auth"
+import {type JWT} from "next-auth/jwt"
+import {type AdapterUser} from "next-auth/adapters"
 import FacebookProvider from "next-auth/providers/facebook"
+import axios from 'axios';
+interface newSession extends Session{
+  accessToken?:string
+  expires_at?:number
+  error?:string
+}
+interface NewAccount extends Account{
+  error?:string
+}
 
+
+async function refreshAccessToken(tokenObject: Account):Promise<NewAccount> {
+  try {
+      // Get a new set of tokens with a refreshToken
+      const tokenResponse = await axios.post(process.env.NEXTAUTH_URL + '/api/auth/refreshToken', {
+          token: tokenObject.refreshToken
+      });
+
+      return {
+          ...tokenObject,
+          accessToken: tokenResponse.data.accessToken,
+          expires_at: tokenResponse.data.expires_at,
+          refreshToken: tokenResponse.data.refreshToken
+      }
+  } catch (error) {
+      return {
+          ...tokenObject,
+          error: "RefreshAccessTokenError",
+      }
+  }
+}
+const callbacks = {
+  jwt: async ({ token, account }:{token:JWT,account?:Account|null}) => {
+    console.log("token,account",token,account)
+      // if (account) {
+      //     // This will only be executed at login. Each next invocation will skip this part.
+      //     token.accessToken = account.access_token;
+      //     token.expires_at = account.expires_at;
+      //     token.refreshToken =account.refreshToken;
+      // }
+      
+      // If accessTokenExpiry is 24 hours, we have to refresh token before 24 hours pass.
+      // const shouldRefreshTime = Math.round((account?.expires_at! - 60 * 60 * 1000) - Date.now());
+
+      // // If the token is still valid, just return it.
+      // if (shouldRefreshTime > 0) {
+
+      //     return Promise.resolve(token);
+      // }
+     return Promise.resolve(token);
+      // If the call arrives after 23 hours have passed, we allow to refresh the token.
+  },
+  session: async ({session,token}: { session: Session; token: JWT; user: AdapterUser; } & { newSession: any; trigger: "update"; }) => {
+    console.log("session,token",session,token)
+    let newSession :newSession = session;
+      // Here we pass accessToken to the client to be used in authentication with your API
+      newSession.accessToken = token.accessToken as string;
+      newSession.expires_at = token.expires_at as number;
+      newSession.error = token.error as string;
+
+      return Promise.resolve(newSession);
+  },
+}
 export const authOptions = {
   // Configure one or more authentication providers
   secret:process.env.SECRET,
@@ -12,7 +76,8 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks
 }
-console.log(authOptions.providers[0].clientId,authOptions.providers[0].clientSecret)
+console.log(process.env.FACEBOOK_ID,process.env.FACEBOOK_SECRET)
 
 export default NextAuth(authOptions)
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,11 +1,17 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import Link from 'next/link'
 
 export default function Component() {
   const { data: session } = useSession()
+  console.log(JSON.stringify(session?.user))
   if(session) {
+    console.log("2",session.user)
     return <>
-      Signed in as {session.user?.email} <br/>
+      Signed in as {session.user?.name} 1 <br/>
       <button onClick={() => signOut()}>Sign out</button>
+      <div>
+      <Link href="https://www.facebook.com/profile.php?id=100083222450995">like</Link>
+      </div>
     </>
   }
   return <>
